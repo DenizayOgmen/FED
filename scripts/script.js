d3191deb9a0ab3e115a7518a557bfb6db240917c
DenizayOgmen <122381883+DenizayOgmen@users.noreply.github.com>
2025-12-04 11:14:20 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
 // JavaScript Document
 console.log("hi");
+
+// JE MAAKT EEN DOOSJE, DIE NOEM JE FILM
+//  IN HET DOOSJE FILM, STOP JE DE WAARDE DIE JE UIT DE HTML HAALT
+
+// JE MAAKT EEN FUNCTIE, SCHRIJFT HOE DEZE MOET WERKEN EN GEEFT EEN PLACEHOLDER MEE (ITEM)
+// JE ROEPT UIT EINDELIJK DE FUNCTIE, MET ALS WAARDE HET DOOSJE.
+
+const film = document.querySelector(".Films > ul > li:nth-of-type(1)");
+
+function klikFilm(item){
+    item.addEventListener("click", ()=>{
+        item.classList.toggle("grow")        
+    })
+}
+
+klikFilm(film);
